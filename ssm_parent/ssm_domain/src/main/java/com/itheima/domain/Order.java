59e80c9a233b387f080280ac54340c616e73349f
fwbbenben <328871765@qq.com>
2019-05-31 18:27:52 +0800

    老师修改了班长了order.java类

--- ssm_parent/ssm_domain/src/main/java/com/itheima/domain/Order.java
+++ ssm_parent/ssm_domain/src/main/java/com/itheima/domain/Order.java
@@ -3,7 +3,7 @@ package com.itheima.domain;
 public class Order {
     private Long orderId;
     //班长加的
-    private String name1;
-    private String orderNumber1;
-
+    private String name;
+    //1.0
+    private  String password;
 }
